936389a97834cf0136b83d36224a7e5aecbb0b59
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-12-21 23:37:10 +0700

    update config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    images: {
-        domains: ['dummyimage.com', 'ui-avatars.com', 'storage.googleapis.com'],
-    },
 }
 
 module.exports = nextConfig
